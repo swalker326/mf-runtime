9ce1b8d1195a35b75eca4628c48111d0eb3dc0b9
Shane <shane@swalker.dev>
2024-12-07 12:28:01 -0600

    fixed typescript error added stub wrapper

--- apps/host/src/System.tsx
+++ apps/host/src/System.tsx
@@ -1,22 +1,33 @@
 import { loadRemote } from "@module-federation/enhanced/runtime";
 import { lazy, Suspense } from "react";
+import { ErrorBoundary } from "react-error-boundary";
+
+const loadRemoteWithZephyr = <T extends { default: React.ComponentType }>(
+	remote: string,
+) => {
+	// Upload remote to Zephyr
+	return lazy(() =>
+		loadRemote<T>(remote).then(
+			(module) =>
+				module ?? {
+					default: () => <h2>Remote not found</h2>,
+				},
+		),
+	);
+};
 
 export const System = ({ remote }: { remote: string }) => {
 	if (!remote) {
 		return <h2>No remote specified</h2>;
 	}
 
-	const Component = lazy(() => {
-		const module = loadRemote<{ default: React.ComponentType }>(remote);
-		if (module === null) {
-			return Promise.resolve(() => <h2>Remote not found</h2>);
-		}
-		return module;
-	});
+	const Component = loadRemoteWithZephyr(remote);
 
 	return (
-		<Suspense fallback="Loading System">
-			<Component />
-		</Suspense>
+		<ErrorBoundary fallback={<h1>Remote Failed to load</h1>}>
+			<Suspense fallback="Loading System">
+				<Component />
+			</Suspense>
+		</ErrorBoundary>
 	);
 };
